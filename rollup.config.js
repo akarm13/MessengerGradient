5e2ac922233bbaf2f15e943230aea102585f01a4
Akar <akarmathers13@gmail.com>
2018-07-16 03:17:46 +0300

    Seperate entry points

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,17 @@
 // rollup.config.js
-export default {
+export default [
+  {
     input: 'src/content/index.js',
     output: {
-      file: 'dist/bundle.js',
+      file: 'dist/content.js',
       format: "iife"
     }
-};
+  },
+  {
+    input: 'src/popup/index.js',
+    output: {
+      file: 'dist/popup.js',
+      format: "iife"
+    }
+  }
+];
