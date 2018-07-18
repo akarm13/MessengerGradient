b825b18ce17b85c8591a5a976d03ad524e80e4d9
Akar <akarmathers13@gmail.com>
2018-07-18 21:34:20 +0300

    Add a function inject the styles

--- src/helpers.js
+++ src/helpers.js
@@ -18,6 +18,15 @@ export function generateGradient(color) {
     return `linear-gradient(135deg, ${color} 0%, ${shadeColor(color, -0.4)} 92%)`;
 }
 
-
-
+/**
+ * Generates a style tag at the begining of the document to 
+ * overide the styles that you select.
+ * 
+ * @param {string} styles A string consisting of the styles that you want to apply.
+ */
+export function injectStyles(styles) {
+    var customStyles = document.createElement('style');
+    customStyles.innerHTML = styles;
+    document.documentElement.insertBefore(customStyles, null);
+}
 
