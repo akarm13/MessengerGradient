88f677e9aac4411dd49224dcc866064b77eb9cb3
Akar <akarmathers13@gmail.com>
2018-07-21 22:24:15 +0300

    Remove shadeColors because we let the user choose the other color of the gradient

--- src/helpers.js
+++ src/helpers.js
@@ -1,16 +1,4 @@
 /**
- * 
- * @param {string} color A hexadecimal value for the color that you want to darken or lighten.
- * @param {number} percent A number from -1.0 to 1.0.
- */
-
-export default function shadeColor(color, percent) {   
-    var f=parseInt(color.slice(1),16),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=f>>16,G=f>>8&0x00FF,B=f&0x0000FF;
-    return "#"+(0x1000000+(Math.round((t-R)*p)+R)*0x10000+(Math.round((t-G)*p)+G)*0x100+(Math.round((t-B)*p)+B)).toString(16).slice(1);
-}
-
-
-/**
  * Generates a gradient using a color then darkens the same color by a certain percent to make a one color gradient.
  * 
  * @param {string} color A hexadecimal value for the first color of the gradient
