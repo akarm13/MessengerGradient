c9009812f19268db8af6a4faa5316043d30518f7
Akar <akarmathers13@gmail.com>
2018-07-27 18:57:50 +0300

    Add populating the inputs

--- src/popup/index.js
+++ src/popup/index.js
@@ -1,43 +1,63 @@
 import  '../../libs/jquery.min.js';
 import { generateGradient } from '../helpers';
 
-let senderGradient;
-let receiverGradient;
+let firstYColor ,secondYColor, 
+    firstTColor ,secondTColor,
+    senderColors, receiverColors;
 
-$('#senderMessage').val(localStorage.getItem('senderColor'));
-$('#receiverMessage').val(localStorage.getItem('receiverColor'));
 
-$('#senderPreview').css('background', generateGradient(localStorage.getItem('senderColor')));
-$('#receiverPreview').css('background', generateGradient(localStorage.getItem('receiverColor')));
+// Set default colors for the storage.
+if (localStorage.getItem('senderColors') === null ||
+    localStorage.getItem('receiverColors') === null) {
+    localStorage.setItem('senderColors', JSON.stringify(['11AABB', '000167']));
+    localStorage.setItem('receiverColors', JSON.stringify(['005767', '9911AA']));
+} else {
+    // The gradient background
+    $('#yGradientPreview').css('background', generateGradient(localStorage.getItem('senderColors')));
+    $('#tGradientPreview').css('background', generateGradient(localStorage.getItem('receiverColors')));
+
+
+    // Populate the inputs with the color values from the session.
+    $('#firstYColor').val(JSON.parse(localStorage.getItem('senderColors'))[0]);
+    $('#secondYColor').val(JSON.parse(localStorage.getItem('senderColors'))[1]);
+    $('#firstTColor').val(JSON.parse(localStorage.getItem('receiverColors'))[0]);
+    $('#secondTColor').val(JSON.parse(localStorage.getItem('receiverColors'))[1]);
+}
 
 const onFormSubmit = (event) => {
-    senderGradient = event.target.elements[0].value;
-    receiverGradient = event.target.elements[1].value;
+     firstYColor  = event.target.elements[0].value;
+     secondYColor = event.target.elements[1].value;
+     firstTColor  = event.target.elements[2].value;
+     secondTColor = event.target.elements[3].value;
+
+     // TODO: Validate the inputs before putting them into the localStorage.
+
+     senderColors = JSON.stringify([firstYColor, secondYColor]);
+     receiverColors = JSON.stringify([firstTColor, secondTColor]);
 
 
-    localStorage.setItem('senderColor', senderGradient);
-    localStorage.setItem('receiverColor', receiverGradient);
+     // Ex: "[131313, 414141]";
+     localStorage.setItem('senderColors', senderColors);
+     localStorage.setItem('receiverColors', receiverColors);
 
+    // Send the inputs to all the tabs.
     chrome.tabs.query({}, tabs => {
         tabs.forEach(tab => {
         chrome.tabs.sendMessage(tab.id, {title: 'formSubmitted', data: {
-            senderGradient,
-            receiverGradient
+            senderColors,
+            receiverColors
         }});
       });
     });
 
     event.preventDefault();
+};
+
+const onInputChange = (event) => {
+    
 }
 
-const onInputChanged = (event) => {
-    if(event.target.id === 'senderMessage') {
-        $('#senderPreview').css('background', generateGradient(event.target.value));
-    } else {
-        $('#receiverPreview').css('background', generateGradient(event.target.value));
-    }
-}
+$('#gradient-picker').on('submit', onFormSubmit);
+$('input').on('keyup', onInputChange);
 
 
-$('#gradientForm').on('submit', onFormSubmit);
-$('.input').on('keyup', onInputChanged);
